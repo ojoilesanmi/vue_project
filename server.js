776b3de63d19ea0a513abcdea057a2c36c1994b5
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-03-25 06:42:58 +0100

    Made changes to server.js to allow the deployment of JSON Server

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
+
+
