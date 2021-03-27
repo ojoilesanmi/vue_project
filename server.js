8c0497856f0801bb2e3cbb72267c987a0b786bf0
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-03-27 20:20:16 +0100

    added a new port to server.js route

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.get(/.*/, function (req, res) {
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; // <== You can change the port
+const middlewares = jsonServer.defaults();// <== You can change the port
 
 server.use(middlewares);
 server.use(router);
